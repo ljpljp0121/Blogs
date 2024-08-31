0b7be756174be544aadf0b2b0e686bf4bd3aacc9
dsaddsa <145369544+ljpljp0121@users.noreply.github.com>
2024-08-31 16:01:18 +0800

    8/31

---
+++ C++PrimerPlus/test.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    cout << "\adsd \n";
+    cout << "code:________\b\b\b\b\b\b";
+    long code;
+    cin >> code;
+    cout << "\asdsd " << code << "...\n";
+    cout << "\aCode   \n";
+    return 0;
+}
